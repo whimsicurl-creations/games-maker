3903c4e9f0b93f7d0c5ab9b1fbc42c46e06d8664
Bethany <blmeier@gmail.com>
2019-08-13 13:55:22 -0400

    finish setting up game JS

--- typing-game/js/main.js
+++ typing-game/js/main.js
@@ -45,10 +45,41 @@ const words = [
 function init() {
     // Load word from array
     showWord(words);
+    // Start matching on word input
+    wordInput.addEventListener('input', startMatch);
     // Call countdown every second
     setInterval(countdown, 1000);
     // Check game status
     setInterval(checkStatus, 50);
+}
+
+// Start match
+function startMatch() {
+    if (matchWords()) {
+        isPlaying = true;
+        time = 6;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+
+    // If score is -1, display 0
+    if (score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;   
+    }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!!!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 // Pick & show randow word
@@ -77,5 +108,6 @@ function countdown() {
 function checkStatus() {
     if(!isPlaying && time === 0) {
         message.innerHTML = 'Game Over!!!';
+        score = -1;
     }
 }
