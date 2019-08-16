ff5a07898e52702942176bdf7d37523ae1ffb15e
Bethany <blmeier@gmail.com>
2019-08-16 11:33:58 -0400

    add scoring

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -4,6 +4,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 function arenaSweep() {
+    let rowCount = 1;
     outer: for (let y = arena.length - 1; y > 0; y--) {
         for (let x = 0; x < arena[y].length; x++) {
             if (arena[y][x] === 0) {
@@ -14,6 +15,9 @@ function arenaSweep() {
         const row = arena.splice(y, 1)[0].fill(0);
         arena.unshift(row);
         y++;
+
+        player.score += rowCount * 10;
+        rowCount *= 2;
     }
 }
 
@@ -123,6 +127,7 @@ function playerDrop() {
         merge(arena, player);
         playerReset();
         arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -142,6 +147,8 @@ function playerReset() {
                     (player.matrix[0].length / 2 | 0);
     if (collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
 
@@ -197,6 +204,10 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
 const colors = [
     null,
     '#FF0D72',
@@ -211,8 +222,9 @@ const colors = [
 const arena = createMatrix(12, 20);
 
 const player = {
-    pos: {x:5, y:5},
-    matrix: createPiece('T'),
+    pos: {x:0, y:0},
+    matrix: null,
+    score: 0,
 }
 
 document.addEventListener('keydown', event => {
@@ -229,4 +241,6 @@ document.addEventListener('keydown', event => {
     }
 })
 
+playerReset();
+updateScore();
 update();
